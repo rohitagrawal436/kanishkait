4974652969ab1edcb029d2b97be74974884a9911
Rohit Agrawal <ra279407@gmail.com>
2021-05-05 20:37:34 +0530

    node js project

---
+++ confi/passportt.js
@@ -0,0 +1,12 @@
+var GoogleStrategy=require("passport-google-oauth20").Strategy;
+var LocalStrategy=require("passport-local").Strategy;
+var passport=require("passport");
+passport.use(new GoogleStrategy({
+clientID:"1013697787793-v8n6brmuh7h0upircls1p25oj1udsjrt.apps.googleusercontent.com",
+clientSecret:"53GfiTAEKknl3KGkuzCT6a35",
+callbackURL:"http://localhost:1102/users/google/auth/callback"
+},()=>{}
+
+
+));
+
